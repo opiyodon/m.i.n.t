32b04886b92c0732b60a608649eec3119594fc80
Don Artkins <opiyodon9@gmail.com>
2023-09-19 04:44:54 +0300

    handled Navbar,react icons,background img, and env

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,21 +1,31 @@
-import './globals.css'
-import Navbar from '/components/Navbar'
-import Footer from "/components/Footer"
+"use client";
 
-export const metadata = {
-  title: 'M.I.N.T',
-  description: 'M.I.N.T is a ToDo app created with Nextjs, Typescript, Tailwind and data is stored in Mongo DB',
-  viewport: 'width=device-width, initial-scale=1',
-}
+import React, { useState } from "react";
+import "./globals.css";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function RootLayout({ children }) {
+
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevDarkMode) => !prevDarkMode);
+  };
+
   return (
-    <html lang="en" className="overflow-x-hidden w-[100%]">
-      <body className="bg-light_bg_bright font-sans text-light_txt transition-all duration-1000 ease-in-out ||| DARK">
-      <Navbar />
+    <html lang="en" className="scroll-smooth">
+      <head>
+        <title>M.I.N.T</title>
+      </head>
+      <body
+        className={`bg-light_bg_bright font-sans text-light_txt transition-all duration-1000 ease-in-out ${
+          darkMode ? "" : "DARK"
+        }`}>
+        <Navbar toggleDarkMode={toggleDarkMode} />
         {children}
-      <Footer />
+        <Footer />
       </body>
     </html>
-  )
+  );
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,58 +1,62 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import { FaSun } from "react-icons/fa";
+import { BsFillMoonStarsFill } from "react-icons/bs";
 
 const Navbar = () => {
+  const [isSunIconVisible, setIsSunIconVisible] = useState(true); // State to toggle the icon
+
+  const toggleIcon = () => {
+    setIsSunIconVisible(!isSunIconVisible);
+    toggleDarkMode(); // Call the toggleDarkMode function on button click
+  };
+
   return (
     <nav className="flex gap-5 p-7 bg-light_bg_dim items-center justify-center top-0 sticky w-[100%]">
-
-
       <div>
-
-
         <Link href={"/"}>
-
-          <div
-            className="flex gap-1 cursor-pointer px-10 justify-center"
-          >
-            {/*<Image
-              src="/bg.jpg"
+          <div className="flex gap-1 cursor-pointer px-10 justify-center">
+            <Image
+              src="/images/bg.jpg"
               alt="logo image"
-              className="rounded-full w-8 h-8 border-2 border-primary transition-all duration-300 cover"
-              width={0}
-              height={0}
-  />*/}
+              width={32}
+              height={32}
+              className="flex flex-col justify-center items-center object-cover rounded-full w-8 h-8 transition-all duration-1000 ease-in-out"
+            />
 
-            <div
-              className="font-bold text-2xl"
-            >
-              M.I<span className="text-primary transition-all duration-300">.N.T</span>
+            <div className="font-bold text-2xl">
+              M.I
+              <span className="text-primary transition-all duration-300">
+                .N.T
+              </span>
             </div>
-
           </div>
-
         </Link>
-
       </div>
-
 
       <div className="flex items-center justify-center px-10">
-
-        <Link href={"/"} className="font-semibold text-lg hover:text-primary transition-all duration-300">Home</Link>
-        
+        <Link
+          href={"/"}
+          className="font-semibold text-lg hover:text-primary transition-all duration-300">
+          Home
+        </Link>
       </div>
-
 
       <div className="flex items-center justify-center px-10">
-
-        <div className="flex items-center justify-center rounded-full w-8 h-8 border-2 border-primary hover:border-secondary text-primary hover:text-secondary cursor-pointer transition-all duration-300">
-          L
-        </div>
-
+        <button
+          className="flex items-center justify-center w-8 h-8 text-primary hover:text-secondary cursor-pointer transition-all duration-300"
+          onClick={toggleIcon}
+          name="light_dark_toggler">
+          {isSunIconVisible ? (
+            <FaSun className="w-6 h-6" />
+          ) : (
+            <BsFillMoonStarsFill className="w-6 h-6" />
+          )}
+        </button>
       </div>
-
-
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
