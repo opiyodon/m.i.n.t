7782f63695c1b17eb4272e110eee458aa55ddc1a
Don Artkins <opiyodon9@gmail.com>
2023-09-19 05:08:14 +0300

    finished the createtodoform and added several todos to database and also handled the routing after creating todo

--- app/api/todos/route.js
+++ app/api/todos/route.js
@@ -2,11 +2,11 @@ import connectToMongoDB from '/libs/mongodb';
 import { NextResponse } from 'next/server';
 import Todo from '/models/todo';
 
-export async function POST(request) {
-  const { title, description } = await request.json();
+export async function POST(req) {
+  const { title, description } = await req.json();
   await connectToMongoDB();
   await Todo.create({ title, description });
-  return NextResponse.json({ message: "Todo Created" }, { status: 201 });
+  return NextResponse.json({ message: "Todo Created." }, { status: 201 });
 }
 
 export async function GET() {
@@ -15,8 +15,8 @@ export async function GET() {
   return NextResponse.json({ todos });
 }
 
-export async function DELETE(request) {
-  const id = request.nextUrl.searchParams.get("id");
+export async function DELETE(req) {
+  const id = req.nextUrl.searchParams.get("id");
   await connectToMongoDB();
   await Todo.findByIdAndDelete(id);
   return NextResponse.json({ message: "Todo Deleted" }, { status: 200 });
--- components/CreateTodoForm.jsx
+++ components/CreateTodoForm.jsx
@@ -1,97 +1,96 @@
 "use client";
 
-import { useState } from 'react';
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 function CreateTodoForm() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [error, setError] = useState("");
 
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
+  const router = useRouter();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        if (!title || !description) {
-            alert('Title and Description are required.');
-            return;
-        }
+    if (!title || !description) {
+        setError("Title and Description are required.");
+      return;
+    }
 
-        /** ========================== post data from database ========================== */
-        fetch('/api/todos', {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json',
-            },
-            body: JSON.stringify({ title, description }),
-        })
-            .then((res) => {
-                if (res.ok) {
-                    // Handle success
-                    console.log('Todo added successfully');
-                    // You can redirect here if needed
-                } else {
-                    // Handle failure
-                    throw new Error('Failed to create Todo');
-                }
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-        /** ========================== post data from database ========================== */
+    /** ========================== post data from database ========================== */
+    try {
+      const res = await fetch("api/todos", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          description,
+        }),
+      });
 
-    };
+      if (res.ok) {
+        const form = e.target;
+        form.reset();
+        router.push("/");
+      } else {
+        setError("Failed to create Todo");
+      }
+    } catch (error) {
+        setError(error);
+    }
+    /** ========================== post data from database ========================== */
+  };
 
-    return (
-        <div className="p-20">
-
-            <form
-                onSubmit={handleSubmit}
-                action=""
-                method="POST"
-                className="flex flex-col gap-4 items-center justify-center w-[500px] h-[100%] bg-light_bg_dim text-light_txt outline-none p-16 border-2 rounded-lg border-light_border_bright border-opacity-50 focus:border-primary"
-            >
-                <div className="flex items-center justify-center">
-                    <h1 className="text-2xl font-semibold text-primary pb-4">
-                        Create Todo
-                    </h1>
-                </div>
-
-                <div className="flex items-center justify-center">
-                    <input
-                        onChange={(e) => setTitle(e.target.value)}
-                        value={title}
-                        type="text"
-                        name="title"
-                        id="title"
-                        placeholder="Enter Todo Title"
-                        className="w-[100%] bg-light_bg_dim text-light_txt outline-none h-10 p-3 border-2 rounded-lg border-light_border_bright border-opacity-50 focus:border-primary"
-                    />
-                </div>
-
-                <div className="flex items-center justify-center">
-                    <input
-                        onChange={(e) => setDescription(e.target.value)}
-                        value={description}
-                        type="text"
-                        name="description"
-                        id="description"
-                        placeholder="Enter Todo Description"
-                        className="w-[100%] bg-light_bg_dim text-light_txt outline-none h-10 p-3 border-2 rounded-lg border-light_border_bright border-opacity-50 focus:border-primary"
-                    />
-                </div>
-
-                <div className="flex items-center justify-center pt-5">
-                    <button
-                        type="submit"
-                        className="py-3 px-16 text-black rounded-full bg-primary font-bold hover:bg-secondary transition-all duration-300 ease-in-out"
-                    >
-                        Add Todo
-                    </button>
-                </div>
-
-            </form>
-
+  return (
+    <div className="p-20">
+      <form
+        onSubmit={handleSubmit}
+        action=""
+        method="POST"
+        className="flex flex-col gap-4 items-center justify-center w-[500px] h-[100%] bg-light_bg_dim text-light_txt outline-none p-16 border-2 rounded-lg border-light_border_bright border-opacity-50 focus:border-primary">
+        <div className="flex items-center justify-center">
+          <h1 className="text-2xl font-semibold text-primary pb-4">
+            Create Todo
+          </h1>
         </div>
-    )
+
+        <div className="flex items-center justify-center">
+          <input
+            onChange={(e) => setTitle(e.target.value)}
+            type="text"
+            placeholder="Enter Todo Title"
+            className="w-[100%] bg-light_bg_dim text-light_txt outline-none h-10 p-3 border-2 rounded-lg border-light_border_bright border-opacity-50 focus:border-primary"
+          />
+        </div>
+
+        <div className="flex items-center justify-center">
+          <input
+            onChange={(e) => setDescription(e.target.value)}
+            type="text"
+            placeholder="Enter Todo Description"
+            className="w-[100%] bg-light_bg_dim text-light_txt outline-none h-10 p-3 border-2 rounded-lg border-light_border_bright border-opacity-50 focus:border-primary"
+          />
+        </div>
+
+        <div className="flex items-center justify-center pt-5">
+          <button
+            type="submit"
+            className="py-3 px-16 text-black rounded-full bg-primary font-bold hover:bg-secondary transition-all duration-300 ease-in-out">
+            Add Todo
+          </button>
+        </div>
+
+        {error && (
+            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+              {error}
+            </div>
+          )}
+      </form>
+    </div>
+  );
 }
 
 export default CreateTodoForm;
