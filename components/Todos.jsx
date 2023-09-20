0735f6bc1cae0fa9e64e4ad7f4dda0ca21a1a889
Don Artkins <opiyodon9@gmail.com>
2023-09-20 15:05:57 +0300

    conditionally rendering todos

--- components/Todos.jsx
+++ components/Todos.jsx
@@ -1,22 +1,22 @@
-import Link from 'next/link';
-import RemoveBtn from './RemoveBtn';
-import { HiPencilAlt } from 'react-icons/hi';
+import Link from "next/link";
+import RemoveBtn from "./RemoveBtn";
+import { HiPencilAlt } from "react-icons/hi";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const getTodos = async () => {
   try {
     const res = await fetch(`${API_URL}/api/todos`, {
-      cache: 'no-store',
+      cache: "no-store",
     });
 
     if (!res.ok) {
-      throw new Error('Failed to fetch todos');
+      throw new Error("Failed to fetch todos");
     }
 
     return res.json();
   } catch (error) {
-    console.error('Error loading todos: ', error);
+    console.error("Error loading todos: ", error);
   }
 };
 
@@ -25,28 +25,29 @@ async function Todos() {
 
   return (
     <>
-      {/* Display the fetched data */}
-      {todos.map((t) => (
-        <div
-          key={t._id} // Add a unique key for each item
-          className="flex justify-between gap-5 items-start w-[500px] bg-light_bg_dim text-light_txt outline-none p-7 border-2 rounded-lg border-light_border_bright border-opacity-50"
-        >
-          <div>
-            <h2 className="font-bold text-2xl text-primary">{t.title}</h2>
-            <div>{t.description}</div>
-          </div>
+      {/*  {todos.map((t) => (
+        
+      ))}  */}
+      {todos &&
+        todos.map((t) => (
+          <div
+            key={t._id} // Add a unique key for each item
+            className="flex justify-between gap-5 items-start w-[500px] bg-light_bg_dim text-light_txt outline-none p-7 border-2 rounded-lg border-light_border_bright border-opacity-50">
+            <div>
+              <h2 className="font-bold text-2xl text-primary">{t.title}</h2>
+              <div>{t.description}</div>
+            </div>
 
-          <div className="flex gap-2">
-            <RemoveBtn id={t._id} />
-            <Link
-              href={`/editTodo/${t._id}`}
-              className="text-primary hover:text-secondary transition-all duration-300 ease-in-out"
-            >
-              <HiPencilAlt size={24} />
-            </Link>
+            <div className="flex gap-2">
+              <RemoveBtn id={t._id} />
+              <Link
+                href={`/editTodo/${t._id}`}
+                className="text-primary hover:text-secondary transition-all duration-300 ease-in-out">
+                <HiPencilAlt size={24} />
+              </Link>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </>
   );
 }
