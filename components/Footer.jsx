2354fad98bd878ed75ae437eec46118fabec210e
Don Artkins <opiyodon9@gmail.com>
2023-09-21 23:15:27 +0300

    updated footer links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,17 @@
+import Link from "next/link"
+
 const Footer = () => {
   return (
     <footer className="flex justify-center p-3 bg-light_bg_dim text-light_txt1">
         <p>
-            &copy;2023 M.I.N.T All rights Reserved | Developed by <span className="text-primary font-semibold cursor-pointer hover:text-secondary transition-all duration-300">Artkins</span>
+            &copy;2023 M.I.N.T All rights Reserved | Developed by <span className="text-primary font-semibold cursor-pointer hover:text-secondary transition-all duration-300">
+              <Link
+                href={"https://donartkins.vercel.app/"}
+                target="_blank"
+              >
+              Artkins
+              </Link>
+            </span>
         </p>
     </footer>
   )
